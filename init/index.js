3c50affdd56edf8b425bdd31ca7ef6ffde35dd7b
Knishka kumari <knishka22k@gmail.com>
2026-05-31 11:21:55 +0530

    Added authentication and user features

---
+++ init/index.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose"); 
+main() 
+.then(() => {  
+    console.log("connection successful"); 
+}) 
+.catch(err => console.log(err));  
+async function main(){  
+    await mongoose.connect(`mongodb://127.0.0.1:27017/wanderlust`);  //mongoose
+}  
+
+const Content = require("../models/content.js");
+const initdata = require("./data.js");
+
+const initDB = async () => {
+    await Content.deleteMany({});
+    await Content.insertMany(initdata.data);
+    console.log("data was initalized");
+};
+initDB();
